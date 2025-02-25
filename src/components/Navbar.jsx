5c73f77c7d4fa557f6457421a73ded367d3fad33
Callum BP <callumbp15@gmail.com>
2025-02-25 12:36:36 +0000

    created Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+
+const Navbar = () => {
+    const styles = {
+        navbar: {
+            backgroundColor: 'lightgray',
+            top: 0,
+            width: '100%',
+            height: '64px',
+            display: 'flex',
+            alignItems: 'center',
+            justifyContent: 'space-between',
+            padding: '0 20px',
+            boxShadow: '0 2px 4px rgba(0,0,0,0.1)'
+        },
+        h1: {
+            color: 'black',
+            margin: 0,
+            fontSize: '24px'
+        }
+    }
+
+    return (
+        <div style={styles.navbar}>
+            <h1 style={styles.h1}>Wishlister</h1>
+        </div>
+    )
+}
+
+export default Navbar;
