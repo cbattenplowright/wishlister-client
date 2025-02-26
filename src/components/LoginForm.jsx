8d0ecac4a886b36401f49a41529e4b8b7ed25c09
Callum BP <callumbp15@gmail.com>
2025-02-26 17:29:27 +0000

    added basic login form element

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,15 +1,31 @@
 import React from 'react';
 
-const LoginForm = ({email, onEmailChange}) => {
+const LoginForm = ({email, onEmailChange, password, onPasswordChange}) => {
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        console.log('Email:', email);
+        console.log('Password:', password);
+    }
+
     return(
         <>
-            <form className="login-form">
+            <form className="login-form" onSubmit={handleSubmit}>
                 <input 
                     type="email"
                     value={email}
                     placeholder="Email address"
                     onChange={(e) => onEmailChange(e.target.value)}
-                    required />
+                    required
+                />
+                <input 
+                    type="password"
+                    value={password}
+                    placeholder="Password"
+                    onChange={(e) => onPasswordChange(e.target.value)}
+                    required
+                />
+                <button type="submit">Login</button>
             </form>
         </>
     )
--- src/containers/LoginContainer.jsx
+++ src/containers/LoginContainer.jsx
@@ -1,15 +1,17 @@
 import React from 'react';
 import {useState} from 'react';
 import LoginForm from '../components/LoginForm';
+import wishlisterLogo from '../assets/wishlisterlogo.svg';
 
 const LoginContainer = () => {
 
     const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
 
     return (
         <div className="login-container">
-            <img src="https://via.placeholder.com/150" alt="wishlister logo" />
-            <LoginForm email={email} onEmailChange={setEmail} />
+            <img src={wishlisterLogo} alt="wishlister logo" />
+            <LoginForm email={email} onEmailChange={setEmail} password={password} onPasswordChange={setPassword}/>
         </div>
     )
 };
