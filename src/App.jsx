5cb740153f87fc7a81a9037c202ef2f838f5576e
Callum BP <callumbp15@gmail.com>
2025-03-27 13:06:07 +0000

    moved AuthProvider outside of the Routes tags as it is not a Route component

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,47 @@
-import { useState } from 'react';
-import ReactDOM from 'react-dom';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import reactLogo from './assets/react.svg';
-import viteLogo from '/vite.svg';
-import './App.css';
-import LoginContainer from './containers/LoginContainer';
-import RegisterContainer from './containers/RegisterContainer';
-import WishlistListContainer from './containers/WishlistListContainer';
-import WishlistProductListContainer from './containers/WishlistProductListContainer';
-import WishlistProductContainer from './containers/WishlistProductContainer';
-import LayoutContainer from './containers/LayoutContainer';
-import AuthProvider from './AuthProvider';
+import { useState } from "react";
+import ReactDOM from "react-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import LoginContainer from "./containers/LoginContainer";
+import RegisterContainer from "./containers/RegisterContainer";
+import WishlistListContainer from "./containers/WishlistListContainer";
+import WishlistProductListContainer from "./containers/WishlistProductListContainer";
+import WishlistProductContainer from "./containers/WishlistProductContainer";
+import LayoutContainer from "./containers/LayoutContainer";
+import AuthProvider from "./hooks/AuthProvider";
+import PrivateRoute from "./router/PrivateRoute";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className='App'>
-      <AuthProvider>
-        <BrowserRouter>
+    <div className="App">
+      <BrowserRouter>
+        <AuthProvider>
           <Routes>
             <Route path="/" element={<LayoutContainer />}>
               <Route index element={<LoginContainer />} />
-              <Route path="/register" element={<RegisterContainer />} />
-              <Route path="/wishlists" element={<WishlistListContainer />} />
-              <Route path="/wishlist/:wishlistId" element={<WishlistProductListContainer />} />
-              <Route path="/wishlist/:wishlistId/product/:productId" element={<WishlistProductContainer />} />
+              <Route element={PrivateRoute}>
+                <Route path="/register" element={<RegisterContainer />} />
+                <Route path="/wishlists" element={<WishlistListContainer />} />
+                <Route
+                  path="/wishlist/:wishlistId"
+                  element={<WishlistProductListContainer />}
+                />
+                <Route
+                  path="/wishlist/:wishlistId/product/:productId"
+                  element={<WishlistProductContainer />}
+                />
+              </Route>
             </Route>
           </Routes>
-          <h1>This is a h1 tag</h1>
-        </BrowserRouter>
-      </AuthProvider>
+        </AuthProvider>
+        <h1>This is a h1 tag</h1>
+      </BrowserRouter>
     </div>
-  )
-};
+  );
+}
 
 export default App;
