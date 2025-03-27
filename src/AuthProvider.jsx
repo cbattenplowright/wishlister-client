85a022be8b97be93f2470a1ca9affb4cb427df70
Callum Batten-Plowright <callum.battenplowright@outlook.com>
2025-03-27 00:23:06 +0000

    added loginUser function

--- src/AuthProvider.jsx
+++ src/AuthProvider.jsx
@@ -1,8 +1,33 @@
-import { useContext, createContext } from "react";
+import { useContext, createContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 const AuthProvider = ({children}) => {
+    const [user, setUser] = useState(null);
+    const navigate = useNavigate();
+
+    const loginUser = async (data) => {
+        try {
+          const response = await fetch("your-api-endpoint/auth/login", {
+            method: "POST",
+            headers: {
+              "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+          });
+          const res = await response.json();
+          if (res.data) {
+            setUser(res.data.user);
+            navigate("/dashboard");
+            return;
+          }
+          throw new Error(res.message);
+        } catch (err) {
+          console.error(err);
+        }
+      };
+
     return ( 
         <AuthContext.Provider>{children}</AuthContext.Provider>
     );
