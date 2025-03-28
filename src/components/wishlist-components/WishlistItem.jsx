60b67f6b6b6f753fd3744361f0c1cea7519a8a32
Callum Batten-Plowright <callum.battenplowright@outlook.com>
2025-03-28 23:11:43 +0000

    added handleClick function to WishlistItem, need to change Backend to return the wishlistId in the response

--- src/components/wishlist-components/WishlistItem.jsx
+++ src/components/wishlist-components/WishlistItem.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import ShareIcon from '@mui/icons-material/Share';
+import { useNavigate } from 'react-router-dom';
 import './WishlistItem.css';
 
 const WishlistItem = ({ wishlistItem }) => {
+
+    const navigate = useNavigate();
+
+    const handleClick = () => {
+        alert(`Clicked on ${wishlistItem.wishlistName}`);
+        navigate(`/wishlist/${wishlistItem.wishlistId}`);
+    }
+
     return (
         <div className="wishlist-item">
-            <img src="https://placehold.co/100" alt="photo of wishlist" />
-            <h2>{wishlistItem.wishlistName}</h2>
-            <button>
-                <ShareIcon />
+            <button className="wishlist-item-button" onClick={handleClick}>
+                <img src="https://placehold.co/100" alt="photo of wishlist" />
+                <h2>{wishlistItem.wishlistName}</h2>
+                <button className="share-button">
+                    <ShareIcon />
+                </button>
             </button>
         </div>
     )   
