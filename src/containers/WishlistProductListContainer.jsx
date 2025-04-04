73163b1b45e0af66d836295d84975d8c3687b6de
Callum BP <callumbp15@gmail.com>
2025-04-04 16:32:45 +0100

    changed url call back to accessing wishlists endpoint

--- src/containers/WishlistProductListContainer.jsx
+++ src/containers/WishlistProductListContainer.jsx
@@ -1,63 +1,69 @@
-import React, { useEffect, useState } from 'react';
-import WishlistProductList from '../components/wishlist-product-list-components/WishlistProductList';
-import { mockWishlistProductItems } from '../mock-data/MockWishlistProductItems';
-import { useAuth } from '../hooks/AuthProvider';
-import { useWishlist } from '../hooks/WishlistProvider';
+import React, { useEffect, useState } from "react";
+import WishlistProductList from "../components/wishlist-product-list-components/WishlistProductList";
+import { mockWishlistProductItems } from "../mock-data/MockWishlistProductItems";
+import { useAuth } from "../hooks/AuthProvider";
+import { useWishlist } from "../hooks/WishlistProvider";
 
 const WishlistProductListContainer = () => {
+  const auth = useAuth();
+  const wishlist = useWishlist();
+  const [wishlistProductItems, setWishlistProductItems] = useState(null);
 
-    const auth = useAuth();
-    const wishlist = useWishlist();
-    const [wishlistProductItems, setWishlistProductItems] = useState(null);
+  const fetchWishlist = async () => {
+    try {
+      if (!auth.user) {
+        console.error("No user found");
+        return;
+      }
 
-    const fetchWishlist = async () => {
-        try {
-            if (!auth.user){
-                console.error("No user found");
-                return;
-            }
+      console.log(
+        `Fetching wishlist for user credentials:, ${auth.user.email}, ${auth.credentials}`
+      );
 
-            console.log(`Fetching wishlist for user credentials:, ${auth.user.email}, ${auth.credentials}`);
-
-            const response = await fetch (
-                // `https://wishlister-h2tf.onrender.com/api/wishlists/${auth.user.userAccountId}`,
-                `http://localhost:8080/api/wishlist-products/user/${auth.user.userAccountId}/${wishlist.wishlistId}`,
-                {
-                    method: "GET",
-                    headers: {
-                        Authorization: `Basic ${btoa(`${auth.user.email}:${auth.credentials}`)}`,
-                    }
-                }
-            )
-
-            if (!response.ok) {
-                throw new Error(`HTTP error! status: ${response.status}`);
-            }
-
-            const res = await response.json();
-            if (res) {
-                setWishlistProductItems(res);
-                console.log(res);
-            }
-        } catch (err) {
-            console.error('Error fetching wishlist:', err);
+      const response = await fetch(
+        // `https://wishlister-h2tf.onrender.com/api/wishlists/${auth.user.userAccountId}`,
+        `http://localhost:8080/api/wishlists/${auth.user.userAccountId}/${wishlist.wishlistId}`,
+        {
+          method: "GET",
+          headers: {
+            Authorization: `Basic ${btoa(
+              `${auth.user.email}:${auth.credentials}`
+            )}`,
+          },
         }
+      );
+
+      if (!response.ok) {
+        throw new Error(`HTTP error! status: ${response.status}`);
+      }
+
+      const res = await response.json();
+      if (res) {
+        setWishlistProductItems(res);
+        console.log(res);
+      }
+    } catch (err) {
+      console.error("Error fetching wishlist:", err);
     }
+  };
 
-    useEffect(() => {
-        if (auth.user) {
-            console.log(auth.user);
-            fetchWishlist();
-        }
-    }, [auth.user]);
+  useEffect(() => {
+    if (auth.user) {
+      console.log(auth.user);
+      fetchWishlist();
+    }
+  }, [auth.user]);
 
-    // const wishlistProductItemsForWishlist1 = wishlistProductItems.filter((wishlistProductItem) => wishlistProductItem.wishlistId === 1);
+  // const wishlistProductItemsForWishlist1 = wishlistProductItems.filter((wishlistProductItem) => wishlistProductItem.wishlistId === 1);
 
-    return (
-        <div className="wishlist-product-list-container">
-            <WishlistProductList wishlistProductItems={wishlistProductItems} wishlistName={"Wishlist no.1"}/>
-        </div>
-    );
-}
+  return (
+    <div className="wishlist-product-list-container">
+      <WishlistProductList
+        wishlistProductItems={wishlistProductItems}
+        wishlistName={"Wishlist no.1"}
+      />
+    </div>
+  );
+};
 
 export default WishlistProductListContainer;
